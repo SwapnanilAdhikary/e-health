5bc15b171e87b87d328f983d44877b9c4b36f17a
Your Name <you@example.com>
2024-09-07 19:22:20 +0530

    Initial commit

---
+++ components/CustomFormField.tsx
@@ -0,0 +1,94 @@
+"use client"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Control } from "react-hook-form"
+import { FormFieldType } from "./forms/PatientForm"
+import Image from "next/image"
+import 'react-phone-number-input/style.css'
+import PhoneInput, { Value } from 'react-phone-number-input'
+interface CustomProps{
+    control: Control<any>,
+    fieldType: FormFieldType,
+    name: string,
+    label?: string,
+    placeholder?: string,
+    iconSrc?: string,
+    iconAlt?: string,
+    disabled?: boolean,
+    dataFormat?: string,
+    showTimeSelect?: boolean,
+    children?: React.ReactNode,
+    renderSkeleton?: (field: any) => React.ReactNode,
+}
+
+const RenderField = ({field, props}:{field: any; props:CustomProps}) =>{
+    const { fieldType, iconSrc , iconAlt, placeholder} = props;
+    switch (fieldType) {
+        case FormFieldType.INPUT:
+            return(
+                <div className="flex rounded-md border border-dark-500 bg-dark-400">
+                    {iconSrc && (
+                    <Image
+                      src={iconSrc}
+                      height={24}
+                      width={24}
+                      alt={iconAlt || 'icon'}
+                      className='ml-2' 
+                    />
+                    )}
+                   <FormControl>
+                    <Input 
+                      placeholder={placeholder}
+                      {...field}
+                      className="shad-input border-0"
+                    />
+                   </FormControl>
+                </div>
+            ) 
+        case FormFieldType.PHONE_INPUT:
+            return(
+                <FormControl>
+                    <PhoneInput
+                        defaultCountry="US"
+                        placeholder={placeholder}
+                        international
+                        withCountryCallingCode
+                        value={field.value as undefined}
+                        onChange={field.onChange}
+                        className="input-phone"                    
+                    />
+                </FormControl>
+            )    
+        default:
+            break;
+    }
+}
+
+const CustomFormField = (props: CustomProps) => {
+    const { control, fieldType, name, label}  = props;
+  return (
+    <FormField
+        control={control}
+        name={name}
+        render={({ field }) => (
+          <FormItem className="flex-1">
+            {fieldType !== FormFieldType.CHECKBOX && label &&(
+                <FormLabel>{label}</FormLabel>
+            )}
+        <RenderField field={field} props={props}/>
+        <FormMessage className="shad-error" />
+          </FormItem>
+        )}
+      />
+  )
+}
+
+export default CustomFormField
---
+++ lib/validation.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const UserFormValidation = z.object({
+    name: z.string()
+      .min(2,"Username must be at least 2 characters.")
+      .max(50,"Name must be at most 100 characters"),
+    email: z.string().email("Invalid email address"),
+    phone: z.string().refine((phone)=>/^\+?[1-9]\d{1,14}$/.test(phone),'Invalid phone number')
+  })
+  
